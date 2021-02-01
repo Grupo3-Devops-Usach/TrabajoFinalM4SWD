4d3b134029ef7ab495ba794644ff370a10e8a8e1
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2021-01-31 22:54:31 -0300

    Se actualiza implementacion de logica de ws y html. Se agregan pruebas unitarias

--- src/main/java/com/devops/dxc/devops/rest/RestData.java
+++ src/main/java/com/devops/dxc/devops/rest/RestData.java
@@ -6,6 +6,7 @@ import com.devops.dxc.devops.model.Dxc;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -17,25 +18,46 @@ public class RestData {
 	private final static Logger LOGGER = Logger.getLogger("devops.subnivel.Control");
 
 	@GetMapping(path = "/dxc", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Dxc> getData(@RequestParam("sueldo") String sueldo, @RequestParam("ahorro") String ahorro){
+	public ResponseEntity<Dxc> getData(@RequestParam("sueldo") @Nullable String sueldo,
+									   @RequestParam("ahorro") @Nullable String ahorro){
 		
 		LOGGER.log(Level.INFO, "< Trabajo DevOps - DXC > <Consultado Diez por ciento>");
 
 		int iAhorro = 0;
 		int iSueldo = 0;
 
-		try{
-			iAhorro = Integer.parseInt(ahorro);
-		} catch (NumberFormatException nex){
+		if(sueldo == null){
 			throw new ResponseStatusException(
-					HttpStatus.BAD_REQUEST, "Parámetro Ahorro debe ser numérico");
+					HttpStatus.BAD_REQUEST, "Parámetro Sueldo es obligatorio");
+		}
+
+		if(ahorro == null){
+			throw new ResponseStatusException(
+					HttpStatus.BAD_REQUEST, "Parámetro Ahorro es obligatorio");
 		}
 
 		try{
 			iSueldo = Integer.parseInt(sueldo);
+
+			if(iSueldo<0){
+				throw new ResponseStatusException(
+						HttpStatus.BAD_REQUEST, "Parámetro Sueldo debe ser mayo a 0");
+			}
 		} catch (NumberFormatException nex){
 			throw new ResponseStatusException(
 					HttpStatus.BAD_REQUEST, "Parámetro Sueldo debe ser numérico");
+		}
+
+		try{
+			iAhorro = Integer.parseInt(ahorro);
+
+			if(iAhorro<0){
+				throw new ResponseStatusException(
+						HttpStatus.BAD_REQUEST, "Parámetro Ahorro debe ser mayo a 0");
+			}
+		} catch (NumberFormatException nex){
+			throw new ResponseStatusException(
+					HttpStatus.BAD_REQUEST, "Parámetro Ahorro debe ser numérico");
 		}
 
 		Dxc dxc = new Dxc(iAhorro, iSueldo);
--- src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
+++ src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
@@ -24,4 +24,58 @@ class DevopsApplicationTests {
 		assertEquals("Retiro máximo", (uf * 150), diezxciento.getDxc());
 	}
 
+	@Test
+	void testSaldoRetiroMaximo(){
+		Dxc diezxciento = new Dxc(70000000, 0);
+		diezxciento.getDxc();
+		int uf = Util.getUf();
+
+		assertEquals("Saldo retiro máximo", (70000000-(uf * 150)), diezxciento.getSaldo());
+	}
+
+	@Test
+	void testRetiroUnMillon(){
+		Dxc diezxciento = new Dxc(2000000, 0);
+
+		assertEquals("Retiro total ahorro", 1000000, diezxciento.getDxc());
+	}
+
+	@Test
+	void testSaldoRetiroUnMillon(){
+		Dxc diezxciento = new Dxc(2000000, 0);
+		diezxciento.getDxc();
+
+		assertEquals("Saldo setiro total ahorro", (2000000-1000000), diezxciento.getSaldo());
+	}
+
+	@Test
+	void testRetiroTotalAhorro(){
+		Dxc diezxciento = new Dxc(900000, 0);
+
+		assertEquals("Retiro total ahorro", 900000, diezxciento.getDxc());
+	}
+
+	@Test
+	void testSaldoRetiroTotalAhorro(){
+		Dxc diezxciento = new Dxc(900000, 0);
+		diezxciento.getDxc();
+
+		assertEquals("Saldo retiro total ahorro", 0, diezxciento.getSaldo());
+	}
+
+	@Test
+	void testRetiroDiezPorciento(){
+		Dxc diezxciento = new Dxc(15000000, 0);
+
+		assertEquals("Retiro 10% ahorro", 1500000, diezxciento.getDxc());
+	}
+
+	@Test
+	void testSaldoRetiroDiezPorciento(){
+		Dxc diezxciento = new Dxc(15000000, 0);
+		diezxciento.getDxc();
+
+		assertEquals("Saldo retiro 10% ahorro", (int)(15000000-(15000000*0.1)), diezxciento.getSaldo());
+	}
+
 }
